eb75fe63c3c6c5fa825729e546578428d0c19340
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2019-05-30 22:03:25 +0530

    code...

--- colour/Assets/scripts/move.cs
+++ colour/Assets/scripts/move.cs
@@ -6,7 +6,7 @@ public class move : MonoBehaviour
 {
     public Vector3 a= Vector3.zero;
     public float fa = 250f;
-    public float sa = 1500f;
+    public float sa = 5f;
     public Rigidbody r;
     // Start is called before the first frame update
     void Start()
@@ -17,8 +17,10 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        r.AddForce(0, 0, sa * Time.deltaTime);
-
+        // r.AddForce(0, 0, sa * Time.deltaTime);
+        //r.transform.Translate(Vector3.forward * Time.deltaTime*sa);
+        Vector3 newPosition = new Vector3(0, 0, 5f);
+        transform.Translate(newPosition * Time.deltaTime);
         if (Input.GetKey(KeyCode.A))
         {
             r.AddForce(-fa * Time.deltaTime, 0, 0);
