cc77fe2c10de2bab4452bfa0e172a2e526d324cc
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2019-06-02 00:27:40 +0530

    bug fix

--- colour/Assets/scripts/score.cs
+++ colour/Assets/scripts/score.cs
@@ -7,18 +7,20 @@ public class score : MonoBehaviour
 {
     public Text a,sec,dis;
     public int s;
-    
+    public Transform p;
+
     void Start()
     {
         a.text = 100.ToString();
         
+
     }
 
     
     void Update()
     {
-        
-       
+
+        dis.text = p.position.z.ToString("0");
     }
     
 }
