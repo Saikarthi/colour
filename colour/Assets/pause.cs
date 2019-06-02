8aad9c087c23d50073177f9f5bb7f13c0f98557a
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2019-06-02 18:00:52 +0530

    pause menu 1

---
+++ colour/Assets/pause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+    public static bool gameispause = false;
+    public GameObject pausemenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void resume()
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+    public void pause1()
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 0f;
+        gameispause = true;
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
