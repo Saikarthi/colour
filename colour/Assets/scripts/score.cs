dc5bff1345c1db6385fb1c8f3a80ba304168fc80
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2019-06-16 20:26:51 +0530

    random ads

--- colour/Assets/scripts/score.cs
+++ colour/Assets/scripts/score.cs
@@ -26,6 +26,7 @@ public class score : MonoBehaviour
     void Update()
     {
         ab = p.position.z+daa.baa;
+      
         dis.text = ab.ToString("0");
         
        // DontDestroyOnLoad(GameObject.find)
