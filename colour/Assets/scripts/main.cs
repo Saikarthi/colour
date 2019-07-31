e75b7c79009949c8c47017ca3d89f18a090a788e
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2019-07-31 23:37:36 +0530

    help tut

--- colour/Assets/scripts/main.cs
+++ colour/Assets/scripts/main.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class main : MonoBehaviour
 {
-  
-   
+
+    public GameObject a, b;
 
     void Update()
     {
@@ -15,10 +15,51 @@ public class main : MonoBehaviour
     }
     public void start()
     {
+        if (PlayerPrefs.GetInt("FIRSTTIMEOPENING", 1) == 1)
+        {
+            SceneManager.LoadScene(1);
+        
 
-             SceneManager.LoadScene(1);
+        PlayerPrefs.SetInt("FIRSTTIMEOPENING", 0);
+
+            //Do your stuff here
+
+        }
+        else
+        {
+            SceneManager.LoadScene(2);
+        }
+
+ 
+
+        
     }
-    
+
+    public void help()
+    {
+
+        a.SetActive(true);
+        Invoke("adad", 5);
+
+
+    }
+    public void adad()
+    {
+
+        a.SetActive(false);
+        b.SetActive(true);
+        Invoke("adad1", 5);
+
+    }
+    public void adad1()
+    {
+        b.SetActive(false);
+      
+       
+
+
+
+    }
     public void quit()
     {
         Application.Quit();
---
+++ colour/Assets/scripts/tut.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class tut : MonoBehaviour
+{
+    public GameObject a, b;
+    // Start is called before the first frame update
+    void Start()
+    {
+       if (PlayerPrefs.GetInt("FIRSTTIMEOPENING", 1) == 1)
+        {
+            Debug.Log("First Time Opening");
+            a.SetActive(true);
+            Invoke("adad", 5);
+            
+
+           PlayerPrefs.SetInt("FIRSTTIMEOPENING", 0);
+
+            //Do your stuff here
+
+       }
+    }
+    public void adad()
+    {
+        
+        a.SetActive(false);
+        b.SetActive(true);
+        Invoke("adad1", 5);
+
+    }
+    public void adad1()
+    {
+        SceneManager.LoadScene(2);
+
+       
+
+    }
+}
