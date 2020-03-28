ed8091edc2a459fcf4f2cee810cec850ffdc8b96
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2020-03-28 23:22:16 +0530

    loading bar

---
+++ colour/Assets/scripts/levelloader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class levelloader : MonoBehaviour
+{
+
+    public GameObject loadscreen;
+    public Slider s;
+    public Text t;
+    public void loadlevel(int sindex)
+    {
+        StartCoroutine(loadasyn(sindex));
+
+
+    }
+
+    IEnumerator loadasyn (int sindex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sindex);
+        loadscreen.SetActive(true);
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            s.value = progress;
+            t.text = progress * 100f + "%";
+
+            yield return null;
+        }
+
+    }
+
+}
