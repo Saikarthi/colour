31bebfbbef90d37bd81eac6177f06bb19322b185
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2020-04-04 22:41:04 +0530

    next level

---
+++ colour/Assets/new update/script/camflow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camflow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 a;
+   
+    void Update()
+    {
+      
+            transform.position = player.position + a;
+      
+    }
+}
