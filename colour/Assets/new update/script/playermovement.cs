d5b49732e6ceb3db602cff7cdff89c6a4df40b16
Saikarthi <41743913+Saikarthi@users.noreply.github.com>
2020-04-04 22:56:18 +0530

    movement 1

---
+++ colour/Assets/new update/script/playermovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public Vector3 a = Vector3.zero;
+    public float fa = 250f;
+    public float sa = 12f;
+    public Rigidbody r;
+    void Update()
+    {
+        Vector3 newPosition = new Vector3(0, 0, sa);
+
+        transform.position += newPosition * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            r.AddForce(-fa * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            r.AddForce(fa * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        float tilt = Input.acceleration.x;
+
+        transform.position += new Vector3(tilt / 10, 0, 0);
+    }
+}
